e2469bb6db32ffc9ca15e6428e30da6444e71574
Mr-Maxraw <maxraw1@yandex.ru>
2020-06-20 17:45:06 +0500

    Updates

---
+++ precache-manifest.e94f694dad022f57fb43fce9f3dadab7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376d3fec7ec7a736777a5b1a65e94947",
+    "url": "/server_switch/index.html"
+  },
+  {
+    "revision": "bf29114f979ebc60366b",
+    "url": "/server_switch/static/css/2.01f4c25d.chunk.css"
+  },
+  {
+    "revision": "bf29114f979ebc60366b",
+    "url": "/server_switch/static/js/2.b4ce533f.chunk.js"
+  },
+  {
+    "revision": "8156c2f6529646ac71cdd1cd65cc3f9a",
+    "url": "/server_switch/static/js/2.b4ce533f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "891dbda6e0503f3ad097",
+    "url": "/server_switch/static/js/main.de13b978.chunk.js"
+  },
+  {
+    "revision": "8eb055a989091190ef14",
+    "url": "/server_switch/static/js/runtime-main.1bd35570.js"
+  },
+  {
+    "revision": "1286a19fc578e6b5a92e6dcb744dec16",
+    "url": "/server_switch/static/media/dxicons.1286a19f.woff2"
+  },
+  {
+    "revision": "147eb6fe61ff3af12cd8763022ae7f24",
+    "url": "/server_switch/static/media/dxicons.147eb6fe.ttf"
+  },
+  {
+    "revision": "2b145cefd95f699e2eecc965fb38c8a8",
+    "url": "/server_switch/static/media/dxicons.2b145cef.woff"
+  }
+]);
